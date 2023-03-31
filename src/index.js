de84540fa73ab817caffd84192f6f913a4626bf9
Esther Lee <115832417+ekylee@users.noreply.github.com>
2023-03-31 14:17:00 -0400

    ain

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 
 
@@ -9,8 +9,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashtagRouter>
+    <HashRouter>
       <App />
-    </HashtagRouter>
+    </HashRouter>
   </React.StrictMode>
 );
